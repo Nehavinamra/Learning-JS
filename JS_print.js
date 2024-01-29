aabbc9a935548ef26b631078a111c36f187d94d9
Neha Vinamra <vinamraneha@gmail.com>
2024-01-29 18:19:54 -0500

    Add files via upload
    Printing in JS

---
+++ JS_print.js
@@ -0,0 +1,14 @@
+//There are three different ways to print a statement in JS
+
+//First method
+
+console.log('Hello World');
+
+//Second method
+
+alert("Hello, World!");
+
+//Third method
+
+document.write('Hello, World!');
+

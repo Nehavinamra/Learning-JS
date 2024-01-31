33976eeb122d85b2e499df7991c2b2c6870d1f6a
Neha Vinamra <vinamraneha@gmail.com>
2024-01-30 23:45:45 -0500

    Add files via upload
    Learn how to take user input

---
+++ user_input.js
@@ -0,0 +1,12 @@
+//To take user input we use the following ways
+
+//First Way by using Prompt
+
+var name = prompt("What is your name? ");
+println("Hello " + name + "!");
+
+//Second way to take user inputas integer by using parseInt
+
+
+
+

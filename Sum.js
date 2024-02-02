0345187a20d50ed4ed9c72e5d08c59875ec4684a
Neha Vinamra <vinamraneha@gmail.com>
2024-02-02 11:23:07 -0500

    Add files via upload
    Program for adding two numbers

---
+++ Sum.js
@@ -0,0 +1,10 @@
+//Program to add two numbers
+
+const num1 = 9;
+const num2 = 4;
+
+// adding two numbers
+const sum = num1 + num2;
+
+// displaying the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);

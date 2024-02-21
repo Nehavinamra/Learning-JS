47362d0b43b8242b07d8898aefa517cd94536b89
Nehavinamra <vinamraneha@gmail.com>
2024-02-20 19:00:53 -0500

    for statement

---
+++ for-statement.js
@@ -0,0 +1,20 @@
+//The syntax for defining a For loop in JS is:
+//for (initialization; condition; afterthought)
+
+//Example 1
+
+for (let i = 0; i < 9; i++) {
+    console.log(i);
+  }
+  //This will print all the numbers between 0 and 9
+  
+
+
+//Example 2
+
+let val = '';
+for (let i = 0; i < 10; i++) {
+  val = val + i;
+}
+console.log(val);
+ // This will print: "0123456789"

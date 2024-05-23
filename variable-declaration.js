c99f0777fec9dc1414f04b22b4baf5281442fcbf
Neha Vinamra <vinamraneha@gmail.com>
2024-05-23 13:34:06 +0530

    Update variable-declaration.js

--- variable-declaration.js
+++ variable-declaration.js
@@ -1,9 +1,24 @@
 //In JS, declaring a variable can be done by four diff ways:
 
-//First Automatic Declaration
+//First: Automatic Declaration
 
 x = 5;
 y = 9;
 z = x+y; 
 
-//Second
+//Second: Using Var
+
+var a = 10;
+//Few imp points:
+//1. Var has a global scope, therefore variable declared even inside of scope can be used outside of it
+//2. Var variables can be redeclared and reassigned
+//3. Var is Hoisted
+
+//Third: Using Let
+
+let a = 10;
+//Few imp points:
+//1. Variables cannot be redeclared within same scope
+//2. variables can be reassigned
+//3. It is not hoisted
+

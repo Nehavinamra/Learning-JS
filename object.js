27c7be875438253098a02b96317bd0a509a08cac
Neha Vinamra <vinamraneha@gmail.com>
2024-05-27 20:39:22 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,17 @@
+//Object in Js is Non-primitive datatype
+//eg:
+const person ={
+  firstName: "Neha",
+  lastName: "Vinamra",
+  age: 22
+};
+
+//The key values of an object can be altered, deleted, added
+
+//Object can also be declared without any properties as:
+const person = new object();
+
+//or even as an empty object:
+const person ={};
+
+//Object property invoked with () returns the property but without returns the function defination

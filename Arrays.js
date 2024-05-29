89d092e0b386dec13474bbc4f9ce40d58242b8c2
Neha Vinamra <vinamraneha@gmail.com>
2024-05-29 23:51:56 +0530

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+//Arrays are objects in JS
+//Syntax:
+
+const cars = ["XUV", "BMW", "Dodge"];
+const nums = [1, 22, 78, 90, 10];
+
+//Arrays can also be defined as object by using "new Array" keyword:
+const cities = new Array ("BOM", "DEL", "GOA");

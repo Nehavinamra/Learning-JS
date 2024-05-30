80c2b785deb7c83146dacb29bbb1a19468a6e794
Neha Vinamra <vinamraneha@gmail.com>
2024-05-30 23:33:41 +0530

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,4 +1,4 @@
-//Arrays are objects in JS
+//Arrays are objects in JS, typeof returns "object"
 //Syntax:
 
 const cars = ["XUV", "BMW", "Dodge"];
@@ -6,3 +6,10 @@ const nums = [1, 22, 78, 90, 10];
 
 //Arrays can also be defined as object by using "new Array" keyword:
 const cities = new Array ("BOM", "DEL", "GOA");
+
+//converting an array to string can be done by using toString() function
+
+//Looping array elements can be done by:
+//1. using for loop
+//2. Using forEach()
+//3. using map()

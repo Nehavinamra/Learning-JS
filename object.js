8ea8d04ace25d88e5e79928a85de8dceb3103381
Nehavinamra <vinamraneha@gmail.com>
2024-06-18 13:10:03 +0530

    fix

--- object.js
+++ object.js
@@ -10,11 +10,11 @@ const person = {
 //The key values of an object can be altered, deleted, added
 
 //Object can also be declared without any properties as:
-const person = new object();
+const eg = new object();
 const arr = new Array("hi", "hola");
 
 //or even as an empty object:
-const person = {};
+const empty = {};
 
 //Object property invoked with () returns the property but without returns the function defination
 

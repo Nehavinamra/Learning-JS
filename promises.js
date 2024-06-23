6b8d2f0e20c9152efb30189ce2e3b7323a044cb4
Neha Vinamra <vinamraneha@gmail.com>
2024-06-23 12:33:34 +0530

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,14 @@
+//Promises are better alternative for Async functions
+//JS promises can be of: 
+//pending: result undefined, fulfilled: result is value, rejected:result is error
+//syntax:
+let myProm = new Promise(function(myResult, myRejected)
+                         { //producing code
+                           myResult(//whatever to print if successful);
+                           myRejected(//whatever to print if rejected); 
+                         });
+myPromise.then( //this is consuming code
+  function(value) {//if successful}
+    function (error) {//if rejected}
+      );
+

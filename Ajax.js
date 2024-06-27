6cfa25c37959f0e0427acb98b22994a411830695
Neha Vinamra <vinamraneha@gmail.com>
2024-06-27 23:51:58 +0530

    Update Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -1,2 +1,6 @@
 //AJAX: Asynchronous javascript and XML
 //Sends XMLHTTPS request to server and displays the data in JS and HTML
+
+//The XMLHttpRequest object can be used to exchange data with a web server behind the scenes. 
+//This means that it is possible to update parts of a web page, without reloading the whole page.
+

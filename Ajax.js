f9ff73a142d9332b0673569df0622bc050e7bfec
Neha Vinamra <vinamraneha@gmail.com>
2024-06-28 23:36:56 +0530

    Update Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -4,3 +4,10 @@
 //The XMLHttpRequest object can be used to exchange data with a web server behind the scenes. 
 //This means that it is possible to update parts of a web page, without reloading the whole page.
 
+//Steps involved: Creating the request, defining callback function, open the XMLHTTP obj, send the XMLHTTP obj
+
+const xhttp = new XMLHttpRequest(); //creating request
+xhttp.onload = function() { //Defining callback function
+}
+xhttp.open("GET", "yourlink_"); //open the request
+xhttp.send(); //send the request to the server

688376ed8a101b0b382de97c8ec469a42e7684bb
Neha Vinamra <vinamraneha@gmail.com>
2024-06-30 23:42:35 +0530

    Update Ajax.js

--- Ajax.js
+++ Ajax.js
@@ -17,3 +17,14 @@ xhttp.send(); //send the request to the server
 //onload: define callback function to be excecuted when request receives an answer
 //To send request to server: Use open and send
 //OPEN SYNTAX: open(method, URL, async)
+//Async: either true or false, default is true as the server doesnt have to wait for the response and can execute scripts, avoid using false
+
+//POST REQUEST:
+//use when update file or DB on server
+//sending large amt of data
+//sending user input; more robust and secure than GET
+
+//GET REQUEST
+//syntax: xttp.open("GET", "URL");
+
+
